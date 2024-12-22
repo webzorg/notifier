e5b540015e8b0136b41cfe9e202087fca034c292
Lasha Abulashvili <labulashvili@gmail.com>
2024-12-22 13:53:15 +0100

    setup mailcatcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,8 +29,11 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: "127.0.0.1", port: 1025 }
+
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Make template changes take effect immediately.
   config.action_mailer.perform_caching = false
